dde0e4d82f2725177bb5d979d00698810e896bf3
popsiqle <popsiqle@hotmail.com>
2022-04-17 10:15:29 +0430

    import nest js microservices

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+  const microservice = app.connectMicroservice({
+    transport: Transport.NATS,
+  });
+  await app.startAllMicroservicesAsync();
+  app.enableCors();
   await app.listen(3000);
+
 }
 bootstrap();
