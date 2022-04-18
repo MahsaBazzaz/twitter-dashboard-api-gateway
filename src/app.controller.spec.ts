d400ad0b2fb09b201dc560fb8a13a608ed97e546
popsiqle <popsiqle@hotmail.com>
2022-04-18 09:56:52 +0430

    test configs

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,17 +1,36 @@
+import { HttpModule } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
+import { KnexModule } from 'nest-knexjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let service: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule,
+        KnexModule.forRoot({
+          config: {
+            client: 'pg',
+            version: '5.7',
+            useNullAsDefault: true,
+            connection: {
+              host: '127.0.0.1',
+              port: 5432,
+              user: 'postgres',
+              password: 'pa$$w0rd',
+              database: 'twitter'
+            },
+          },
+        })],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    service = app.get<AppService>(AppService);
   });
 
   describe('root', () => {
